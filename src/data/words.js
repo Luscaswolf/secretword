e5989ea5eeba0c8d22537dbcdaea2440f69de4ef
Lucas Silva <lucaspsilvadev@gmail.com>
2023-03-07 22:40:48 -0300

    Secret_word

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Cérebro", "Pescoço", "Olhos"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Lingaugem", "Frameworlf", "JavaScript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"]
+};
